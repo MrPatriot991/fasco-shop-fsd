8491ae22e7d2314920c7c2ff64bbba093f2c084e
Dmitrii0777 <patrrriot991@gmail.com>
2026-01-17 18:11:35 +0200

    feat: setup axios base client for fakestoreapi

---
+++ src/shared/api/baseApi.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const baseApi = axios.create({
+  baseURL: "https://fakestoreapi.com",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  }
+})
