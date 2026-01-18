9d76130181bcd37eb1d2264f37eb91a664914577
Dmitrii0777 <patrrriot991@gmail.com>
2026-01-18 22:17:01 +0200

    feat(shared): add container ui, theme colors and fonts setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,9 @@
+import { HomePage } from "@/pages/home/ui/HomePage";
+
 function App() {
   return (
     <>
-      <h1 className="font-bold text-4xl underline text-center p-20">FASCO 😁</h1>
+      <HomePage />
     </>
   );
 }
--- src/entities/product/ui/ProductCard.tsx
+++ src/entities/product/ui/ProductCard.tsx
@@ -7,22 +7,22 @@ export const ProductCard = ({ product }: { product: Product }) => {
   return (
     <article className="flex space-y-5 flex-col p-4 h-full bg-white w-full  md:min-w-1/2 rounded-2xl shadow overflow-hidden hover:shadow-lg cursor-pointer transition-shadow duration-200">
       <figure>
-        <img src={image} alt={brand} className="h-60 w-full object-contain" />
+        <img src={image} alt={brand} className="h-60 w-full object-cover" />
       </figure>
       <div className="flex space-y-4 justify-between w-full flex-col flex-1">
         <header className="flex justify-between items-start gap-4">
           <div className="min-w-0">
-            <h3 className="text-xl text-stone-700 font-medium line-clamp-1">{title}</h3>
-            <p className="text-xs text-gray-400 font-medium">{brand}</p>
+            <h3 className="text-xl text-brand-dark font-medium line-clamp-1">{title}</h3>
+            <p className="text-xs text-brand-gray font-medium">{brand}</p>
           </div>
           <span className="text-yellow-400">{"★".repeat(Math.round(rating.rate))}</span>
         </header>
-        <span className="text-xs text-stone-700 font-medium">
+        <span className="text-xs text-brand-dark font-medium">
           ({rating.count}k) Customer Reviews
         </span>
         <footer className="flex justify-between items-center">
-          <strong className="text-xl">{formatCurrency(price)}</strong>
-          {rating.count > 200 && <span className="text-xs text-red-500">Almost Sold Out</span>}
+          <strong className="text-xl text-brand-dark">{formatCurrency(price)}</strong>
+          {rating.count > 200 && <span className="text-xs text-accent-red">Almost Sold Out</span>}
         </footer>
       </div>
     </article>
