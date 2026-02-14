2e4372e69d56936ebe7bf222b99f19c9f602efed
Dmitrii0777 <patrrriot991@gmail.com>
2026-02-14 22:43:47 +0200

    feat(newsletter): refactor NewsLetter component to use Section and SectionTitle for improved structure and semantics

--- src/widgets/news-latter/ui/NewsLetter.tsx
+++ src/widgets/news-latter/ui/NewsLetter.tsx
@@ -1,49 +1,44 @@
-import { Container } from "@/shared/ui";
+import { Section, SectionTitle } from "@/shared/ui";
 import { SubscribeForm } from "@/features/subscribe-newsletter";
-
 import guyImg from "@/shared/assets/images/sub-img-1.png";
 import girlImg from "@/shared/assets/images/sub-img-2.png";
 
 export const NewsLetter = () => {
   return (
-    <section className="bg-brand-ehite pt-20 md:pt-36 pb-12 border-b">
-      <Container maxWidth="3xl">
-        <div
-          className="
-            grid
-            grid-cols-1
-            items-center
-            gap-12
-            lg:grid-cols-[1fr_minmax(0,500px)_1fr]
-          "
-        >
-          {/* Left image */}
-          <div className="hidden lg:block">
-            <img src={guyImg} aria-hidden="true" alt="" className="h-full w-full object-cover" />
-          </div>
+    <Section containerSize="3xl" className="bg-white border-b">
+      <div className="grid grid-cols-1 items-center gap-12 lg:grid-cols-[1fr_minmax(0,500px)_1fr]">
+        {/* Left image */}
+        <div className="hidden lg:block">
+          <img src={guyImg} aria-hidden="true" alt="" className="h-full w-full object-cover" />
+        </div>
 
-          {/* Content */}
-          <div className="flex flex-col items-center gap-6 text-center ">
-            <h2 className="font-volkhov text-section-title leading-tight tracking-tight text-brand-dark">
-              Subscribe To Our Newsletter
-            </h2>
+        {/* Content */}
+        <div className="flex flex-col items-center gap-6 text-center ">
+          <SectionTitle
+            as="h2"
+            variant="section"
+            align="center"
+            subJustify="center"
+            subContent={
+              <p className="font-volkhov max-w-4/5 sm:w-2/3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices
+                sollicitudin aliquam sem.
+              </p>
+            }
+          >
+            Subscribe To Our Newsletter
+          </SectionTitle>
 
-            <p className="max-w-md text-brand-gray">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices
-              sollicitudin aliquam sem.
-            </p>
-
-            <div className="w-full">
-              <SubscribeForm />
-            </div>
-          </div>
-
-          {/* Right image */}
-          <div className="hidden lg:block">
-            <img src={girlImg} aria-hidden="true" alt="" className="h-full w-full object-cover" />
+          <div className="w-full">
+            <SubscribeForm />
           </div>
         </div>
-      </Container>
-    </section>
+
+        {/* Right image */}
+        <div className="hidden lg:block">
+          <img src={girlImg} aria-hidden="true" alt="" className="h-full w-full object-cover" />
+        </div>
+      </div>
+    </Section>
   );
 };
