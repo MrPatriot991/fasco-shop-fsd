1626679adc350080891a29f80833641937fde4fa
Dmitrii0777 <patrrriot991@gmail.com>
2026-02-14 19:43:19 +0200

    feat(deals): enhance Deals component with Section and SectionTitle for improved layout and semantics

--- src/widgets/deals/ui/Deals.tsx
+++ src/widgets/deals/ui/Deals.tsx
@@ -1,9 +1,7 @@
 import { Link } from "react-router-dom";
-import { Button, Container, ErrorBoundary } from "@/shared/ui";
+import { Button, ErrorBoundary, Section, SectionTitle, TimerDisplay, AppSlider } from "@/shared/ui";
 import { useAppSelector } from "@/shared/lib/hooks";
-import { AppSlider } from "@/shared/ui";
 import { selectOnlyDeals } from "@/entities/product";
-import { TimerDisplay } from "@/shared/ui";
 import type { EmblaOptionsType } from "embla-carousel";
 
 const OPTIONS: EmblaOptionsType = { loop: true, align: "center" };
@@ -12,39 +10,51 @@ export const Deals = () => {
   const discountDeals = useAppSelector(selectOnlyDeals);
 
   return (
-    <section id="deals" className="bg-brand-secondary py-16 lg:py-36">
-      <Container>
-        <div className="flex flex-col lg:flex-row gap-20 overflow-hidden">
-          <div className="flex flex-col items-center gap-12 lg:w-2/5">
-            <div className="flex flex-col items-center lg:items-start gap-10">
-              <h2 className="text-[44px] font-volkhov text-brand-dark">Deals Of The Month</h2>
-              <p className="text-center lg:text-start text-brand-gray sm:w-2/3 lg:w-full">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices
-                sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin{" "}
-              </p>
-              <Button asChild>
-                <Link to="/shop?collection=discount-deals">Buy Now</Link>
-              </Button>
-            </div>
-            <div className="flex flex-col gap-6 items-center lg:items-start">
-              <p className="font-medium text-2xl text-center lg:text-start text-brand-dark">
-                Hurry, Before It’s Too Late!
-              </p>
-              <TimerDisplay targetDate="2026-02-28" />
-            </div>
+    <Section id="deals" className="bg-brand-secondary">
+      <div className="flex flex-col lg:flex-row gap-12 lg:gap-20 overflow-hidden">
+        <div className="flex flex-col items-center gap-12 lg:w-2/5">
+          <div className="flex flex-col items-center lg:items-start gap-4 md:gap-6 lg:gap-10">
+            <SectionTitle
+              variant="section"
+              align="center"
+              alignLg="left"
+              subJustify="center"
+              subContent={
+                <p className="text-center sm:w-2/3 lg:w-full lg:text-start">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices
+                  sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin
+                </p>
+              }
+            >
+              Deals Of The Month
+            </SectionTitle>
+            <Button asChild>
+              <Link
+                to="/shop?collection=discount-deals"
+                className="text-[clamp(0.875rem,1vw+0.5rem,1rem)]"
+              >
+                Buy Now
+              </Link>
+            </Button>
           </div>
-          <ErrorBoundary>
-            <AppSlider
-              slides={discountDeals}
-              options={OPTIONS}
-              showButtons
-              height="36rem"
-              maxWidth="lg"
-              sliderSize="55%"
-            />
-          </ErrorBoundary>
+          <div className="flex flex-col gap-6 items-center lg:items-start">
+            <p className="font-medium text-[clamp(1.5rem,2vw+1rem,1.75rem)] text-center lg:text-start text-brand-dark">
+              Hurry, Before It’s Too Late!
+            </p>
+            <TimerDisplay targetDate="2026-02-28" />
+          </div>
         </div>
-      </Container>
-    </section>
+        <ErrorBoundary>
+          <AppSlider
+            slides={discountDeals}
+            options={OPTIONS}
+            showButtons
+            height="36rem"
+            maxWidth="lg"
+            sliderSize="55%"
+          />
+        </ErrorBoundary>
+      </div>
+    </Section>
   );
 };
