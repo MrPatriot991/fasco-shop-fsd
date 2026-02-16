eb836220100c6157b34fdb5fbd2040bc52c711c5
Dmitrii0777 <patrrriot991@gmail.com>
2026-02-16 19:13:32 +0200

    refactor(checkout): add form validation using react-hook-form and zodResolver

--- src/widgets/checkout/ui/CheckoutContact.tsx
+++ src/widgets/checkout/ui/CheckoutContact.tsx
@@ -1,10 +1,20 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Input, SectionTitle } from "@/shared/ui";
+import { checkoutSchema, type CheckoutSchema } from "@/features/checkout";
 
 interface CheckoutContactProps {
   accountPrompt?: React.ReactNode;
 }
 
 export const CheckoutContact = ({ accountPrompt }: CheckoutContactProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm<CheckoutSchema>({
+    resolver: zodResolver(checkoutSchema),
+  });
+
   return (
     <>
       <SectionTitle
@@ -17,7 +27,14 @@ export const CheckoutContact = ({ accountPrompt }: CheckoutContactProps) => {
       >
         Contact
       </SectionTitle>
-      <Input type="email" variant="outline" className="lg:py-6" placeholder="Email Address" />
+      <Input
+        {...register("email")}
+        type="email"
+        variant="outline"
+        className="lg:py-6"
+        placeholder="Email Address"
+        error={errors.email?.message}
+      />
     </>
   );
 };
