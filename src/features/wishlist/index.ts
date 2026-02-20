95f5348817a51e34f6627923c491c7b3f7758371
Dmitrii0777 <patrrriot991@gmail.com>
2026-02-20 16:53:43 +0200

    refactor(features-wishlist): update exports to renamed wishlist model

--- src/features/wishlist/index.ts
+++ src/features/wishlist/index.ts
@@ -1,3 +1,1 @@
-export * from "../../entities/wishlist/model/wishlistSlice";
-export { wishlistSlice } from "../../entities/wishlist/model/wishlistSlice";
 export { ToggleWishlistButton } from "./ui/ToggleWishlistButton";
